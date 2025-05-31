87c3cbffb1d898c678332e035bc0a56ac3602def
callmedeci <callmedecim@gmail.com>
2025-05-31 15:00:56 -0700

    remove VERCEL_URL from base URL fallback

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,11 +6,5 @@ export function merge(...classNames: string[]) {
 }
 
 export function getBaseUrl() {
-  if (typeof window !== 'undefined') return '';
-
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-
-  if (process.env.NEXT_PUBLIC_BASE_URL) return process.env.NEXT_PUBLIC_BASE_URL;
-
-  return 'http://localhost:3000';
+  return process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 }
