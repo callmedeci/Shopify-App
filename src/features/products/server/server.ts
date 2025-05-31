4e8d0c50cdb242a4534099a982248f955b0c2251
callmedeci <callmedecim@gmail.com>
2025-05-31 14:55:36 -0700

    [Add] no-store cache to fetch products for fresh data

--- src/features/products/server/server.ts
+++ src/features/products/server/server.ts
@@ -4,7 +4,7 @@ import { getBaseUrl } from '@/utils/utils';
 
 export async function getProducts(): Promise<ProductsType> {
   const baseUrl = getBaseUrl();
-  const res = await fetch(`${baseUrl}/api/products`);
+  const res = await fetch(`${baseUrl}/api/products`, { cache: 'no-store' });
   if (res.status !== 200) throw new Error('Failed to fetch products');
 
   const data = await res.json();
@@ -14,7 +14,9 @@ export async function getProducts(): Promise<ProductsType> {
 
 export async function getProductById(id: string): Promise<ProductType> {
   const baseUrl = getBaseUrl();
-  const res = await fetch(`${baseUrl}/api/products/${id}`);
+  const res = await fetch(`${baseUrl}/api/products/${id}`, {
+    cache: 'no-store',
+  });
   if (res.status !== 200) notFound();
 
   const data = await res.json();
